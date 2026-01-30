41bdf0a9245ed420e126ce95a7435d3ebfd2a022
Malte Weyrich <malte.weyrich@hotmail.de>
2026-01-30 20:17:10 +0100

    setting up analysis

--- src/run_analysis.sh
+++ src/run_analysis.sh
@@ -6,16 +6,15 @@ PLOT_OUT="/nfs/data3/CIRCEST/runs/test_new_benchmarking/tidy_benchmarking/result
 
 
 # 1. Filter length 
-# python3 ./preprocessing/filter_length.py $PREFIX_GSE138734
-# python3 ./preprocessing/filter_min_threshold.py $PREFIX_GSE138734
-# python3 ./preprocessing/filter_length.py $PREFIX_DEEP
-# python3 ./preprocessing/filter_min_threshold.py $PREFIX_DEEP
+python3 ./preprocessing/filter_length.py $PREFIX_GSE138734
+python3 ./preprocessing/filter_min_threshold.py $PREFIX_GSE138734
+python3 ./preprocessing/filter_length.py $PREFIX_DEEP
+python3 ./preprocessing/filter_min_threshold.py $PREFIX_DEEP
 
 
 # 2. Sort, Merge, Intersect
-#python3 ./preprocessing/bedtools.py $PREFIX_GSE138734
-#python3 ./preprocessing/bedtools.py $PREFIX_DEEP
-
+python3 ./preprocessing/bedtools.py $PREFIX_GSE138734
+python3 ./preprocessing/bedtools.py $PREFIX_DEEP
 
 python3 ./overview.py $PLOT_OUT
 
