edf427cfc1ae45bc8908012d86eaf4f312b8087a
Malte Weyrich <malte.weyrich@hotmail.de>
2026-06-01 10:03:03 +0200

    updating filter evidence script

--- src/preprocessing/filter_min_threshold.py
+++ src/preprocessing/filter_min_threshold.py
@@ -1,14 +1,13 @@
 import os
-import json
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import sys
+import csv
 
 
 DATA_DIR=sys.argv[1]
 if DATA_DIR[-1] != "/":
     DATA_DIR = f"{DATA_DIR}/"
+
+stats = []
 
 for t in ["blacklist"]:
     root_dirs = [
@@ -16,10 +15,10 @@ for t in ["blacklist"]:
         f"{DATA_DIR}total/filtered_bed_{t}/circexplorer2_filtered_{t}/",
         f"{DATA_DIR}polya/filtered_bed_{t}/segemehl_filtered_{t}/",
         f"{DATA_DIR}total/filtered_bed_{t}/segemehl_filtered_{t}/",
-        f"{DATA_DIR}polya/filtered_bed_{t}/dcc_filtered_{t}/",
-        f"{DATA_DIR}total/filtered_bed_{t}/dcc_filtered_{t}/",
-        f"{DATA_DIR}polya/filtered_bed_{t}/ciriquant_filtered_{t}/",
-        f"{DATA_DIR}total/filtered_bed_{t}/ciriquant_filtered_{t}/",
+        f"{DATA_DIR}polya/filtered_bed_{t}/circtools_filtered_{t}/",
+        f"{DATA_DIR}total/filtered_bed_{t}/circtools_filtered_{t}/",
+        f"{DATA_DIR}polya/filtered_bed_{t}/ciri_filtered_{t}/",
+        f"{DATA_DIR}total/filtered_bed_{t}/ciri_filtered_{t}/",
         f"{DATA_DIR}polya/filtered_bed_{t}/find_circ_filtered_{t}/",
         f"{DATA_DIR}total/filtered_bed_{t}/find_circ_filtered_{t}/",
         f"{DATA_DIR}polya/filtered_bed_{t}/circrna_finder_filtered_{t}/",
@@ -28,18 +27,18 @@ for t in ["blacklist"]:
     cutoff = 5
 
     columns = ["filtered", "tool", "origin", "total", "pct", "remaining", "type"]
-    df = pd.DataFrame(columns=columns)
 
     for root_dir in root_dirs:
-        i = 0
-        total = 0
-        origin = root_dir.split("/")[0]
-
+        origin = root_dir.split("/")[-4]
         for filename in os.listdir(root_dir):
+            retained = 0
+            skipped = 0
+            total = 0
             if filename.endswith(".bed"):
                 input_path = os.path.join(root_dir, filename)
                 tool_name = root_dir.split("/")[-2]
                 sample = filename.split("/")[-1].split(".")[0].split("_")[0]
+                sample_name = filename.split("/")[-1].split(".")[0]
                 output_filename = os.path.join(root_dir.replace("bed", "bed_min"), filename)
                 output_path = output_filename.replace(
                     f"{tool_name}_filtered", f"{tool_name}_filtered_min"
@@ -63,13 +62,22 @@ for t in ["blacklist"]:
                         try:
                             score = int(parts[4])
                             if score < cutoff:
-                                i += 1
+                                skipped += 1
                             else:
+                                retained += 1
                                 outfile.write(line)
                         except ValueError:
                             continue
+            stats.append((origin, tool_name, sample_name, skipped, retained, total, retained/total))
 
-        tool_name = root_dir.split("/")[-2]
-        print(f"Stats for min evidence filtering for {root_dir}")
-        print(f"Retained: {total-i}, Removed: {i} Ratio: {i/total}")
-        df.loc[len(df)] = [i, tool_name, origin, total, i / total, total - i, t]
+        print(f"Stats for min evidence filtering for {sample_name}")
+        print(f"Retained: {retained}, Removed: {skipped} Ratio: {retained/total}")
+
+
+col_names = ["origin", "tool", "sample", "skipped", "retained", "total", "ratio"]
+
+out_path = os.path.join(DATA_DIR, "evidence_filter_stats.tsv")
+with open(out_path, "w", newline="") as f:
+    writer = csv.writer(f, delimiter="\t")
+    writer.writerow(col_names)
+    writer.writerows(stats)
